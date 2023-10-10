5fd9d1effa1ecf6778ce78510eed300b5252cce9
kubaglowik <jakubgo45@gmail.com>
2023-10-10 13:04:15 +0200

    Create AtomInput

---
+++ todoapp/src/components/Atoms/AtomInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <input type="text"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    >
+</template>
+<script>
+export default {
+  name: 'AtomInput',
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: String,
+      default: ''
+    }
+  },
+  emits: ['update:modelValue']
+}
+</script>
+<style>
+  
+</style>
