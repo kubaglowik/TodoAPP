0722a784014eb15eeaf70e51bd1100fdd4e18bc4
kubaglowik <jakubgo45@gmail.com>
2023-10-10 13:05:00 +0200

    Added Atom buttons and imput

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="item">
-    <input type="text" placeholder="add task" v-model="newitem" />
+    <atom-input type="text" placeholder="add task" v-model="newitem"></atom-input>
     <button class="additem" @click="addItem">add</button>
-    <select name="sort" id="sort">
+    <select name="sort" id="sort" @change="change">
       <option value="az">a-z</option>
       <option value="za">z-a</option>
       <option value="stn">old to new</option>
@@ -13,32 +13,26 @@
     class="item"
     v-bind:class="{ status: item.status }"
     v-for="item in items"
-    v-bind:key="item.id"
-  >
+    v-bind:key="item.id">
     <h2>{{ item.title }}</h2>
-    <button class="additem" v-if="!item.status" @click="removeitem(item.id)">
-      ready
-    </button>
-    <button
-      class="delete"
-      v-if="item.avilable == 0"
-      @click="deleteeitem(item.id)"
-    >
-      delete
-    </button>
-  </div>
-  <div id="app">
-    <MyComponent />
+    <atom-button v-text="'Ready'" class="additem" v-if="!item.status" @click="removeitem(item.id)"></atom-button>
+    <atom-button v-text="'Delete'" class="delete" v-if="item.avilable == 0" @click="deleteeitem(item.id)"></atom-button>
   </div>
 </template>
 
 <script>
-import MyComponent from "./components/MyComponent.vue";
+import AtomButton from './components/Atoms/AtomButton.vue';
+import AtomInput from "./components/Atoms/AtomInput.vue";
+
 
 export default {
+  components: {
+    AtomInput, AtomButton
+  },
+
   data() {
     return {
-      newitem: "default",
+      newitem: "",
       items: [],
     };
   },
@@ -59,10 +53,9 @@ export default {
       const index = this.items.findIndex((el) => el.id === id);
       this.items.splice(index, 1).avilable = 1;
     },
-  },
-  name: "App",
-  components: {
-    MyComponent,
+    // change(){
+    //   if(items==0)
+    // },
   },
 };
 </script>
