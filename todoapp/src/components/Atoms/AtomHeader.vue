ae5694ac20be5cfeadcbf96338ba9e56e3f226f3
kubaglowik <jakubgo45@gmail.com>
2023-10-10 14:08:35 +0200

    created AtomHeader

---
+++ todoapp/src/components/Atoms/AtomHeader.vue
@@ -0,0 +1,18 @@
+<template>
+<h2>{{text}}</h2>
+</template>
+<script>
+export default {
+   name: 'AtomHeader',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+  },
+
+}
+</script>
+<style>
+  
+</style>
