038847252399ccea89054136065bd5064f4dd4c8
kubaglowik <jakubgo45@gmail.com>
2023-10-10 14:37:34 +0200

    Create AtomOption

---
+++ todoapp/src/components/AtomOption.vue
@@ -0,0 +1,14 @@
+<template>
+<option :value="modelValue">{{text}}</option>
+</template>
+<script>
+export default {
+      modelValue: {
+      type: String,
+      default: ''
+}
+}
+</script>
+<style>
+  
+</style>
