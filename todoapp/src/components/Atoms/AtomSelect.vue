7064294134c39fcf1dbf64bc754e6d26dcd33529
kubaglowik <jakubgo45@gmail.com>
2023-10-10 14:23:41 +0200

    AtomSelect fix

---
+++ todoapp/src/components/Atoms/AtomSelect.vue
@@ -0,0 +1,20 @@
+<template>
+<select :name="modelName" :id="modelValue">
+  <slot></slot>
+</select>
+</template>
+<script>
+export default {
+      modelName: {
+      type: String,
+      default: ''
+    },
+      modelId:{
+        type: String,
+        default: ''
+      }
+}
+</script>
+<style>
+  
+</style>
