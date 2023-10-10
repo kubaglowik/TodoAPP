8ecd358bb75182c53e70fb384f5d26c722e055bd
kubaglowik <jakubgo45@gmail.com>
2023-10-10 13:03:58 +0200

    created AtomButton

---
+++ todoapp/src/components/Atoms/AtomButton.vue
@@ -0,0 +1,16 @@
+<template>
+<button @input="$emit('@click')">{{text}}</button>
+</template>
+<script>
+export default {
+   name: 'AtomButton',
+   text:{
+    type:String,
+    default: " "
+   }
+
+}
+</script>
+<style>
+  
+</style>
