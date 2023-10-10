708654836a04763090f562de1df9b4a7b193a852
kubaglowik <jakubgo45@gmail.com>
2023-10-10 12:41:28 +0200

    Added select

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="item">
-    <input type="text" placeholder="add task" v-model="newitem">
+    <input type="text" placeholder="add task" v-model="newitem" />
     <button class="additem" @click="addItem">add</button>
+    <select name="sort" id="sort">
+      <option value="az">a-z</option>
+      <option value="za">z-a</option>
+      <option value="stn">old to new</option>
+      <option value="nts">new to old</option>
+    </select>
   </div>
-  <div class="item" v-bind:class="{ status: item.status }" v-for="item in items" v-bind:key="item.id">
+  <div
+    class="item"
+    v-bind:class="{ status: item.status }"
+    v-for="item in items"
+    v-bind:key="item.id"
+  >
     <h2>{{ item.title }}</h2>
-    <button class="additem" v-if="!item.status" @click="removeitem(item.id)">ready</button>
-    <button class="delete" v-if="item.avilable==0" @click="deleteeitem(item.id)">delete</button>
+    <button class="additem" v-if="!item.status" @click="removeitem(item.id)">
+      ready
+    </button>
+    <button
+      class="delete"
+      v-if="item.avilable == 0"
+      @click="deleteeitem(item.id)"
+    >
+      delete
+    </button>
   </div>
   <div id="app">
     <MyComponent />
@@ -14,40 +33,43 @@
 </template>
 
 <script>
-import MyComponent from './components/MyComponent.vue'
+import MyComponent from "./components/MyComponent.vue";
 
 export default {
   data() {
     return {
       newitem: "default",
-      items: [
-      ]
-    }
+      items: [],
+    };
   },
   methods: {
     addItem() {
-      this.items.push({ title: this.newitem, status: false, id: Math.random(), avilable:0})
+      this.items.push({
+        title: this.newitem,
+        status: false,
+        id: Math.random(),
+        avilable: 0,
+      });
     },
     removeitem(id) {
-      const index = this.items.findIndex(el => el.id === id)
-      this.items[index].status = true
+      const index = this.items.findIndex((el) => el.id === id);
+      this.items[index].status = true;
     },
     deleteeitem(id) {
-      const index = this.items.findIndex(el => el.id === id)
-      this.items.splice(index,1).avilable = 1
+      const index = this.items.findIndex((el) => el.id === id);
+      this.items.splice(index, 1).avilable = 1;
     },
   },
-  name: 'App',
+  name: "App",
   components: {
-    MyComponent
-  }
-}
+    MyComponent,
+  },
+};
 </script>
 <style>
 template {
-  display: flex;  
+  display: flex;
   justify-content: center;
-
 }
 
 .main {
@@ -80,11 +102,10 @@ template {
 .additem {
   margin: 10px;
 }
-.delete{
-color: red;
+.delete {
+  color: red;
 }
-.avilable
-{
-  background-color:red;
+.avilable {
+  background-color: red;
 }
 </style>
