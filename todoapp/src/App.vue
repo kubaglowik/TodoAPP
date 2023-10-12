31e447ad6108b79f83c5704daf0c983ae194c1a3
kubaglowik <jakubgo45@gmail.com>
2023-10-12 11:47:28 +0200

    added date display

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -1,21 +1,23 @@
 <template>
-<div class="all">
-  <div class="item">
-    <!-- <p>{{showData}}</p> -->
-    <atom-input type="text" class="text" placeholder="add task" v-model="newitem" @keyup.enter="addItem"></atom-input>
-    <atom-button :text="'ADD'" class="additem" @click="addItem"></atom-button>
-    <atom-select name="sort" id="sort" @change="sortList">
-      <atom-option :text="'a-z'" value="az"/>
-      <atom-option :text="'z-a'" value="za"/>
-      <atom-option :text="'old to new'" value="stn"/>
-      <atom-option :text="'new to old'" value="nts"/>
-    </atom-select>
-  </div>
-  <div class="item2" v-bind:class="{ status: item.status }" v-for="item in items" v-bind:key="item.id">
-    <atom-header :text="item.title"/>
-    <atom-button :text="'Ready'" class="additem" v-if="!item.status" @click="removeitem(item.id)"></atom-button>
-    <atom-button :text="'Delete'" class="delete" v-if="item.avilable === 0" @click="deleteeitem(item.id)"></atom-button>
-  </div>
+  <div class="all">
+    <div class="item">
+      <atom-input type="text" class="text" placeholder="add task" v-model="newitem" @keyup.enter="addItem"></atom-input>
+      <atom-button :text="'ADD'" class="additem" @click="showDataandadditem"></atom-button>
+      <atom-select name="sort" id="sort" @change="sortList">
+        <atom-option :text="'a-z'" value="az" />
+        <atom-option :text="'z-a'" value="za" />
+        <atom-option :text="'old to new'" value="stn" />
+        <atom-option :text="'new to old'" value="nts" />
+      </atom-select>
+    </div>
+    <div class="item2" v-bind:class="{ status: item.status }" v-for="item in items" v-bind:key="item.id">
+      <div class="data">
+      <p>{{ item.data2 }}</p>
+      </div>
+      <atom-header :text="item.title" />
+      <atom-button :text="'Ready'" class="additem" v-if="!item.status" @click="removeitem(item.id)"></atom-button>
+      <atom-button :text="'Delete'" class="delete" v-if="item.avilable === 0" @click="deleteeitem(item.id)"></atom-button>
+    </div>
   </div>
 </template>
 
@@ -29,22 +31,24 @@ import AtomSelect from './components/Atoms/AtomSelect.vue';
 
 export default {
   components: {
-    AtomInput, AtomButton,AtomHeader,AtomSelect,AtomOption
+    AtomInput, AtomButton, AtomHeader, AtomSelect, AtomOption
   },
 
   data() {
     return {
       newitem: "",
       items: [],
+      isDataAvailable: false, // Dodaj tę zmienną
     };
   },
   methods: {
-    
+
     addItem() {
-      this.items.push({ title: this.newitem, status: false, id: Math.random(), avilable: 0, data: new Date,
+      this.items.push({
+        title: this.newitem, status: false, id: Math.random(), avilable: 0, data:new Date(),data2: new Date().toLocaleString(),
       });
-       localStorage.setItem('items', JSON.stringify(this.items));
-        this.newitem = ""; 
+      localStorage.setItem('items', JSON.stringify(this.items));
+      this.newitem = "";
     },
     removeitem(id) {
       const index = this.items.findIndex((el) => el.id === id);
@@ -56,40 +60,40 @@ export default {
       this.items.splice(index, 1).avilable = 1;
       localStorage.setItem('items', JSON.stringify(this.items));
     },
-    sortList(event){
-          const msec = Date.parse(new Date);
+    sortList(event) {
+      const msec = Date.parse(new Date);
       console.log(msec)
       const selectValue = event.target.value;
-      if(selectValue === 'za'){
+      if (selectValue === 'za') {
         this.items = this.items.sort((item1, item2) => {
-          if(item1.title > item2.title) {
+          if (item1.title > item2.title) {
             return -1;
           }
 
           return 1;
         })
       }
-      if(selectValue === 'az'){
+      if (selectValue === 'az') {
         this.items = this.items.sort((item1, item2) => {
-          if(item1.title < item2.title) {
+          if (item1.title < item2.title) {
             return -1;
           }
 
           return 1;
         })
       }
-      if(selectValue === 'stn'){
+      if (selectValue === 'stn') {
         this.items = this.items.sort((item1, item2) => {
-          if(item1.data < item2.data) {
+          if (item1.data < item2.data) {
             return -1;
           }
 
           return 1;
         })
       }
-      if(selectValue === 'nts'){
+      if (selectValue === 'nts') {
         this.items = this.items.sort((item1, item2) => {
-          if(item1.data > item2.data) {
+          if (item1.data > item2.data) {
             return -1;
           }
 
@@ -98,12 +102,13 @@ export default {
       }
     },
 
-    // showData() {
-    //   const msec = Date.parse(new Date);
-    //   console.log(msec)
-    //   const itemdata = (new Date)
-    //   console.log(itemdata);
-    // },
+    showData() {
+
+    },
+    showDataandadditem() {
+      this.showData();
+      this.addItem();
+    },
 
   },
   mounted() {
@@ -129,64 +134,73 @@ export default {
   background: rgb(0, 213, 255);
   background: linear-gradient(176deg, rgba(0, 213, 255, 1) 0%, rgba(0, 127, 255, 1) 100%);
 }
+
 .item {
-  
+
   margin-top: 5%;
   padding: 10px;
   width: 80%;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color:rgb(255, 255, 255);
+  background-color: rgb(255, 255, 255);
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   border-radius: 10px;
 }
+
 .item2 {
-margin: 10px;
+  margin: 10px;
   padding: 10px;
-  min-width: 500px;
+  min-width: 70%;
   width: fit-content;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  font-family: "Montserrat",sans-serif !important;
+  font-family: "Montserrat", sans-serif !important;
   font-weight: 1000;
-  background-color:rgb(255, 255, 255);
+  background-color: rgb(255, 255, 255);
   border-radius: 15px;
 }
-.text{
+
+.text {
   width: 50%;
   padding: 5px 5px;
   border: none;
   border-radius: 4px;
   background-color: #f1f1f1;
 }
-#sort{
+
+#sort {
   width: 20%;
   padding: 5px 5px;
   border: none;
   border-radius: 10px;
   background-color: #f1f1f1;
 }
-.additem{
+
+.additem {
   width: 10%;
   padding: 5px 5px;
   border: none;
   border-radius: 4px;
   background-color: #f1f1f1;
 }
+
 .status {
   opacity: 70%;
 }
+
 .status h2 {
   text-decoration: line-through;
   color: rgb(255, 0, 0);
 }
+
 .additem {
   margin: 10px;
 }
+
 .delete {
   color: red;
   width: 10%;
@@ -194,71 +208,78 @@ margin: 10px;
   border: none;
   border-radius: 4px;
   background-color: #f1f1f1;
-  
+
 }
 
-  * {
-    scrollbar-width: none;
-    scrollbar-color: #00aaff #ffffff;
-  }
+* {
+  scrollbar-width: none;
+  scrollbar-color: #00aaff #ffffff;
+}
 
-  *::-webkit-scrollbar {
-    width: 6px;
-  }
+*::-webkit-scrollbar {
+  width: 6px;
+}
 
-  *::-webkit-scrollbar-track {
-    background: #ffffff;
-  }
+*::-webkit-scrollbar-track {
+  background: #ffffff;
+}
 
-  *::-webkit-scrollbar-thumb {
-    background-color: #00aaff;
-    border-radius: 9px;
-    border: 0px solid #ffffff;
-  }
-  @media screen and (min-width: 425px) {
+*::-webkit-scrollbar-thumb {
+  background-color: #00aaff;
+  border-radius: 9px;
+  border: 0px solid #ffffff;
+}
+
+@media screen and (min-width: 425px) {
   .item {
     width: 80%;
   }
-  
+
   .item2 {
-    min-width: 80%;
+    min-width: 70%;
   }
+
   .additem {
-  width: 15%;
+    width: 15%;
+  }
+
+  .delete {
+    width: 15%;
+  }
 }
-.delete {
-  width: 15%;
-}
-}
+
 @media screen and (min-width: 375px) {
   .item {
     width: 80%;
   }
-  
+
   .item2 {
-    min-width: 80%;
+    min-width: 70%;
   }
+
   .additem {
-  width: 15%;
+    width: 15%;
+  }
+
+  .delete {
+    width: 15%;
+  }
 }
-.delete {
-  width: 15%;
-}
-}
+
 @media screen and (min-width: 320px) {
   .item {
     width: 80%;
   }
-  
-  .item2 {
-    min-width: 80%;
-  }
-  .additem {
-  width: 15%;
-}
-.delete {
-  width: 15%;
-}
-}
 
-</style>
+  .item2 {
+    min-width: 70%;
+  }
+
+  .additem {
+    width: 15%;
+  }
+
+  .delete {
+    width: 15%;
+  }
+}</style>
