e4560af27a3dc52efb1f7c3c19f0a87d8d1b4093
kubaglowik <jakubgo45@gmail.com>
2023-10-13 08:52:56 +0200

    Atom option fix

---
+++ todoapp/src/components/Atoms/AtomOption.vue
@@ -0,0 +1,20 @@
+<template>
+  <option :value="modelValue">{{ text }}</option>
+</template>
+<script>
+export default {
+      name: 'AtomOption',
+      props:{
+      modelValue: {
+      type: String,
+      default: ''
+      },
+      text: {
+      type: String,
+      default: ''
+      }
+}
+}
+</script>
+<style>
+</style>
